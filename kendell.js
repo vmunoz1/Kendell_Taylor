521577fb9037d43c480adfa37eeae1182ff04281
vmunoz1 <45017690+vmunoz1@users.noreply.github.com>
2019-11-20 14:24:50 -0800

    Update kendell.js

--- kendell.js
+++ kendell.js
@@ -12,8 +12,8 @@ var j = document.getElementById("title")
 
 
 function titleChange(elem){
-	h.src="images/Kendell1.jpg";
-	i.src="images/Kendell2.jpg";
+	h.src="Images/Kendell1.jpg";
+	i.src="Images/Kendell2.jpg";
 	a.classList.remove("opaque");
 	b.classList.remove("opaque");
 	c.classList.remove("opaque");
@@ -25,21 +25,21 @@ function titleChange(elem){
 }
 
 function changeHairone(elem){
-	h.src="images/Kendellh1.jpg";
+	h.src="Images/Kendellh1.jpg";
 	b.classList.remove("opaque");
 	c.classList.remove("opaque");
 	a.classList.add("opaque");
 }
 
 function changeHairtwo(elem){
-	h.src="images/Kendellh2.jpg";
+	h.src="Images/Kendellh2.jpg";
 	a.classList.remove("opaque");
 	c.classList.remove("opaque");
 	b.classList.add("opaque");
 }
 
 function changeHairthree(elem){
-	h.src="images/Kendellh3.jpg";
+	h.src="Images/Kendellh3.jpg";
 	a.classList.remove("opaque");
 	b.classList.remove("opaque");
 	c.classList.add("opaque");
@@ -48,7 +48,7 @@ function changeHairthree(elem){
 function playOne(elem){
 	var audio= new Audio('Talking/firstRecord.m4a');
 	audio.play();
-	i.src="images/Kendell1.gif";
+	i.src="Images/Kendell1.gif";
 	d.classList.add("opaque");  
 	e.classList.remove("opaque");  
 	f.classList.remove("opaque");  
@@ -58,7 +58,7 @@ function playOne(elem){
 function playTwo(elem){
 	var audio= new Audio('Talking/secondRecord.m4a');
 	audio.play();
-	i.src="images/Kendell2.gif";
+	i.src="Images/Kendell2.gif";
 	e.classList.add("opaque");
 	d.classList.remove("opaque");  
 	f.classList.remove("opaque");  
@@ -68,7 +68,7 @@ function playTwo(elem){
 function playThree(elem){
 	var audio= new Audio('Talking/thirdRecord.m4a');
 	audio.play();
-	i.src="images/Kendell3.gif";
+	i.src="Images/Kendell3.gif";
 	f.classList.add("opaque");
 	d.classList.remove("opaque");  
 	e.classList.remove("opaque");  
@@ -78,7 +78,7 @@ function playThree(elem){
 function playFour(elem){
 	var audio= new Audio('Talking/fourthRecord.m4a');
 	audio.play();
-	i.src="images/Kendell4.gif";
+	i.src="Images/Kendell4.gif";
 	g.classList.add("opaque");
 	d.classList.remove("opaque");  
 	e.classList.remove("opaque");  
